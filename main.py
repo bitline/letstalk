1638f6793f04ad969a05e335de755bb97eb6cb17
svmalvaez <svmalvaez@gmail.com>
2014-11-01 04:28:06 -0600

    Primer listener testing

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-
 import leap
+import sys
+
+
+class TradukiListener(leap.Listener):
+    def on_connect(self, controller):
+        print "Connected"
+
+    def on_frame(self, controller):
+        frame = controller.frame()
+
+        print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d, tools: %d, gestures: %d" % (
+          frame.id, frame.timestamp, len(frame.hands), len(frame.fingers), len(frame.tools), len(frame.gestures()))
+
+def main():
+    listener = TradukiListener()
+    controller = leap.Controller()
+
+    controller.add_listener(listener)
+    try:
+        sys.stdin.readline()
+    except:
+        pass
+    finally:
+        controller.remove_listener(listener)
+
+main()
